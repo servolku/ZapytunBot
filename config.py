08cd6d3f5589b93c17f5309946ffb81e2df97468
servolku <34325559+servolku@users.noreply.github.com>
2025-04-14 04:42:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ TELEGRAM_BOT_TOKEN = "8121382718:AAFYlfyE9jprJ1tw7Ppb0dPP5AAxxv4oNdQ"
 DATABASE_URL = "sqlite:///quest_bot.db"
 
 # Web Application URL
-WEB_APP_URL = "http://localhost:5000"
+WEB_APP_URL = "http://localhost:8000"
