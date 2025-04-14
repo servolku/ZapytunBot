314be233504f4bd2514d605e8b7ca27f789e13ea
servolku <34325559+servolku@users.noreply.github.com>
2025-04-14 06:54:46 +0300

    Update config.py
    виставив абсолютну адресу БД

--- config.py
+++ config.py
@@ -4,7 +4,8 @@
 TELEGRAM_BOT_TOKEN = "8121382718:AAFYlfyE9jprJ1tw7Ppb0dPP5AAxxv4oNdQ"
 
 # Database Configuration
-DATABASE_URL = "sqlite:///quest_bot.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Абсолютний шлях до кореневої папки проекту
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'quest_bot.db')}"
 
 # Web Application URL
 WEB_APP_URL = "http://localhost:8000"
