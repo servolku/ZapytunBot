61329a9147137c05363fdba03db55d2a459c8c15
servolku <34325559+servolku@users.noreply.github.com>
2025-04-14 04:21:06 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -11,16 +11,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is the Zapytun web app!"
-
-def leaderboard():
-    """Render the leaderboard page."""
     leaderboard_data = get_leaderboard()
-    return render_template("leaderboard.html", leaderboard=leaderboard_data)
+    return render_template("index.html", bot_name="ZapytunBot", leaderboard=leaderboard_data)
 
 if __name__ == "__main__":
-    # Використовуйте порт, який Render надає через змінну середовища PORT
     import os
-    port = int(os.environ.get("PORT", 8000))  # За замовчуванням 5000
-    app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
