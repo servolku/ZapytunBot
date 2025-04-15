4b2d8b6d83f2fe83dcdf3cc260a43fc6e0856df1
servolku <34325559+servolku@users.noreply.github.com>
2025-04-15 10:29:13 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,16 +1,13 @@
 import os
 import logging
-from telegram import Bot
-from telegram import Update
+from telegram import Bot, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-
 import asyncio
-
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/../"))
 
 # Імпортуємо обробники
-from handlers import start, leaderboard, handle_answer, handle_location
+from handlers import start, leaderboard, handle_answer, handle_location, handle_button_click
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
@@ -19,7 +16,6 @@ if not TELEGRAM_BOT_TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN is not set! Перевірте змінні середовища.")
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
-# Асинхронне видалення вебхука
 async def remove_webhook():
     try:
         webhook_deleted = await bot.delete_webhook(drop_pending_updates=True)
@@ -42,43 +38,22 @@ app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 # Додаємо обробники команд
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("leaderboard", leaderboard))
-
-# Додаємо обробник для обробки відповідей користувача через callback_query
+app.add_handler(CallbackQueryHandler(handle_button_click, pattern="get_question"))
 app.add_handler(CallbackQueryHandler(handle_answer))
-
-# Додаємо обробник для обробки геолокації
 app.add_handler(MessageHandler(filters.LOCATION, handle_location))
-
-# Додайте цей обробник для діагностики
-async def debug(update: Update, context):
-    logger.info(f"Received update: {update}")
-    if update.message:
-        logger.info(f"Message text: {update.message.text}")
-        await update.message.reply_text(f"Debug: Received your message: {update.message.text}")
-    elif update.callback_query:
-        logger.info("Callback received!")
-        await update.callback_query.answer("Debug: Callback received!")
-    else:
-        logger.info("Unknown update type.")
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Debug: Unknown update type.")
-
-app.add_handler(MessageHandler(filters.ALL, debug))
 
 # Головна функція для запуску бота
 def main():
     logger.info("Starting bot...")
 
-    # Створення нового циклу подій для уникнення DeprecationWarning
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    # Видалення вебхука перед запуском
     try:
         loop.run_until_complete(remove_webhook())
     except Exception as e:
         logger.error(f"Error while removing webhook: {e}")
 
-    # Запуск polling
     try:
         app.run_polling()
     except Exception as e:
